b18c21395de88c9efa6a38afa819d02e1bc0ab1d
emma.ou <emma.ou@ucloud.cn>
2021-11-09 19:54:31 +0800

    feat:finished preparation

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -5,7 +5,7 @@ const path = require("path");
 const semver = require("semver");
 const colors = require("colors/safe");
 const rootCheck = require("root-check");
-const userHome = require("user-home");
+const { homedir } = require("os");
 const pathExists = require("path-exists").sync;
 const minimist = require("minimist");
 
@@ -13,9 +13,10 @@ const log = require("@emma-cli-develop/log");
 const pkg = require("../package.json");
 const constant = require("./const");
 
+const userHome = homedir();
 let args, config;
 
-function core() {
+async function core() {
   try {
     checkPkgVersion();
     checkNodeVersion();
@@ -23,21 +24,57 @@ function core() {
     checkUserHome();
     checkInputArgs();
     checkEnv();
+    await checkGlobalUpdate();
   } catch (e) {
     log.error(e.message);
   }
 }
+
+async function checkGlobalUpdate() {
+  // 获取当前版本号和模块名
+  const currentVersion = pkg.version;
+  const npmName = pkg.name;
+  // 调用npm API，获取所有版本号
+  // 提取所有版本号，比对哪些版本号大于当前版本号
+  // 获取最新的版本号，提示用户更新到该版本
+  const { getNpmSemverVersion } = require("@emma-cli-develop/get-npm-info");
+  const latestVersion = await getNpmSemverVersion(currentVersion, npmName);
+  if (latestVersion && semver.gt(latestVersion, currentVersion)) {
+    log.warn(
+      "更新提示",
+      colors.yellow(
+        `请手动更新 ${npmName},当前版本：${currentVersion},最新版本：${latestVersion}
+        更新命令：npm install -g ${npmName}`
+      )
+    );
+  }
+}
+
 function checkEnv() {
   const dotenv = require("dotenv");
   const dotenvPath = path.resolve(userHome, ".env");
-  console.log(dotenvPath);
+  // 从 .env 文件中加载配置到 process.env 中
   if (pathExists(dotenvPath)) {
-    config = dotenv.config({
+    dotenv.config({
       path: dotenvPath,
     });
   }
+  config = createDefaultConfig();
+  log.verbose("环境变量缓存文件路径", process.env.CLI_HOME_PATH);
+}
 
-  log.verbose("环境变量", config);
+function createDefaultConfig() {
+  const cliConfig = {
+    home: userHome,
+  };
+
+  if (process.env.CLI_HOME) {
+    cliConfig.cliHomePath = path.join(userHome, process.env.CLI_HOME);
+  } else {
+    cliConfig.cliHomePath = path.join(userHome, constant.DEFAULT_CLI_HOME);
+  }
+  process.env.CLI_HOME_PATH = cliConfig.cliHomePath;
+  return cliConfig;
 }
 
 function checkInputArgs() {
@@ -51,7 +88,7 @@ function checkArgs() {
   } else {
     process.env.LOG_LEVEL = "info";
   }
-  log.level = process.env.LOG_LEVEL;
+  log.level = process.env.LOG_LEVEL; // 由于log在上面require的时候已经初始化过了，当时env.LOG_LEVEL还没有生效，所以需要重新设置log level
 }
 
 function checkUserHome() {
@@ -61,7 +98,7 @@ function checkUserHome() {
 }
 
 function checkRoot() {
-  rootCheck();
+  rootCheck(); // 避免权限问题。如果是root,则尝试降低权限，核心就是使用了process.setgid 和 process.setuid
 }
 
 function checkNodeVersion() {
--- utils/log/lib/index.js
+++ utils/log/lib/index.js
@@ -3,7 +3,7 @@
 const log = require("npmlog");
 
 log.level = process.env.LOG_LEVEL ? process.env.LOG_LEVEL : "info";
-log.heading = "emma";
+log.heading = "emma-cli";
 log.headingStyle = { fg: "red", bg: "white" };
 log.addLevel("success", 2000, { fg: "green", bg: "black" });
 module.exports = log;
